af771b7023c0b0468a2b3a40136607a0340e43a7
Edilawit Legesse <edlawitlegesse@gmail.com>
2026-05-09 21:50:40 +0300

    updated queries.js

--- mongodb/queries.js
+++ mongodb/queries.js
@@ -1,119 +1,93 @@
-use("property_rental");
+use property_rental;
 
 
-db.createCollection("users", {
-  validator: {
-    $jsonSchema: {
-      bsonType: "object",
-      required: ["fullName", "email", "password", "role"],
-      properties: {
-        fullName: { bsonType: "string" },
-        email: { bsonType: "string" },
-        password: { bsonType: "string" },
-        role: { enum: ["owner", "tenant"] }
-      }
-    }
-  }
-});
-
-db.createCollection("locations", {
-  validator: {
-    $jsonSchema: {
-      bsonType: "object",
-      required: ["city", "subcity"],
-      properties: {
-        city: { bsonType: "string" },
-        subcity: { bsonType: "string" }
-      }
-    }
-  }
-});
-
-db.createCollection("properties", {
-  validator: {
-    $jsonSchema: {
-      bsonType: "object",
-      required: ["title", "propertyType", "price", "userId", "locationId"],
-      properties: {
-        title: { bsonType: "string" },
-        propertyType: { bsonType: "string" },
-        price: { bsonType: "number" },
-        status: { enum: ["available", "rented", "not_available"] }
-      }
-    }
-  }
-});
-
+db.createCollection("users");
+db.createCollection("locations");
+db.createCollection("properties");
 db.createCollection("propertyPhotos");
 
 
-
-const user1 = db.users.insertOne({
-  fullName: "Edilawit Legesse",
-  email: "edel@gmail.com",
-  phone: "0945678909",
-  password: "hashed_pw",
-  role: "owner",
-  createdAt: new Date()
-}).insertedId;
-
-const user2 = db.users.insertOne({
+const userResult = db.users.insertMany([
+   {
   fullName: "Eden Alemayehu",
   email: "eden@gmail.com",
-  phone: "0911897589",
+  phone: "0956748932",
   password: "hashed_pw",
   role: "owner",
-  createdAt: new Date()
-}).insertedId;
+  createdAt: new Date("2026-05-01")
+},
+
+{
+  fullName: "Ashenafi Gebremedhn",
+  email: "ashenafi@gmail.com",
+  phone: "0946848962",
+  password: "hashed_pw",
+  role: "tenant",
+  createdAt: new Date("2026-05-02")
+}
+]);
+
+const u1 = userResult.insertedIds[0]; 
+const u2 = userResult.insertedIds[1];
 
 
 
-const loc1 = db.locations.insertOne({
+const locResult = db.locations.insertMany([
+  {
   city: "Addis Ababa",
   subcity: "Bole"
-}).insertedId;
+},
 
-const loc2 = db.locations.insertOne({
+ {
   city: "Addis Ababa",
   subcity: "Goro"
-}).insertedId;
+}
+  ]);
+
+const l1 = locResult.insertedIds[0]; 
+const l2 = locResult.insertedIds[1];
 
 
 
-const prop1 = db.properties.insertOne({
+const propResult = db.properties.insertMany([
+  {
   title: "2 Bedroom Apartment",
-  description: "Clean and modern",
+  description: "Clean and modern apartment",
   propertyType: "apartment",
   price: 15000,
   bedrooms: 2,
   areaSize: 120,
   status: "available",
-  userId: user1,
-  locationId: loc1,
+  userId: u1,
+  locationId: l1,
   createdAt: new Date()
-}).insertedId;
-
-const prop2 = db.properties.insertOne({
+},
+{
   title: "Family House",
-  description: "Spacious with parking",
+  description: "Spacious house with parking",
   propertyType: "house",
   price: 25000,
   bedrooms: 3,
   areaSize: 200,
   status: "rented",
-  userId: user2,
-  locationId: loc2,
+  userId: u1,
+  locationId: l2,
   createdAt: new Date()
-}).insertedId;
+}
+]);
+
+const p1 = propResult.insertedIds[0];
+const p2 = propResult.insertedIds[1];
 
 
 
 db.propertyPhotos.insertMany([
-  { photoUrl: "https://img.com/p1.jpg", propertyId: prop1 },
-  { photoUrl: "https://img.com/p2.jpg", propertyId: prop2 }
+  { photoUrl: "https://img.com/p1.jpg", propertyId: p1 },
+  { photoUrl: "https://img.com/p2.jpg", propertyId: p2 }
 ]);
 
 
+db.users.find();
 
 db.properties.find({ status: "available" });
 
@@ -133,3 +107,5 @@ db.properties.aggregate([
 ]);
 
 db.properties.find({ bedrooms: { $gte: 2 } });
+
+db.locations.find();
